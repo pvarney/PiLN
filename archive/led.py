bbc88b5bf8d5df8304a57b90291caf5c60c2bf6f
Patrick Varney <me@patrickvarney.com>
2017-01-24 19:54:01 -0600

    Removed arch/log dirs

--- archive/led.py
+++
@@ -1,7 +0,0 @@
-import time
-import RPi.GPIO as GPIO ## Import GPIO library
-GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
-GPIO.setup(7, GPIO.OUT) ## Setup GPIO Pin 7 to OUT
-GPIO.output(7,True) ## Turn on GPIO pin 7
-time.sleep(5)
-GPIO.output(7,False) ## Turn on GPIO pin 7
